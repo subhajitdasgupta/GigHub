c891d41029c0d7560f4cef147c59c8720796b8a7
Subhajit Dasgupta <dasguptasubhajitbally@gmail.com>
2023-07-13 16:28:16 +0530

    Creating Models, DbContext, Added Data to Table

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int id { get; set; }
+        [Required]
+        public ApplicationUser Artist { get; set; }
+        [Required]
+        public DateTime DateTime { get; set; }
+        [Required]
+        public String Venue { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
