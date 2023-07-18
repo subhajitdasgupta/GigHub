0d1f37d4bb54a3d7e587b248998d7f9ce798f781
Subhajit Dasgupta <dasguptasubhajitbally@gmail.com>
2023-07-18 16:36:31 +0530

    Added A Gig View.

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,28 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+       private readonly ApplicationDbContext _dbContext;
+        public GigsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var ViewModel = new GigFormViewModel
+            {
+                Genres = _dbContext.Genres.ToList()
+            };
+            return View(ViewModel);
+        }
+    }
+}
